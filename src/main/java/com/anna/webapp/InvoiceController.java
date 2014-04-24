229a5f1d9eff0e0583a4725ed51cf9550ae5c341
Alvaro VM <alvarovm@gmail.com>
2014-04-24 11:37:45 +1000

    commit project

--- src/main/java/com/anna/webapp/InvoiceController.java
+++ src/main/java/com/anna/webapp/InvoiceController.java
@@ -6,6 +6,7 @@ import java.text.SimpleDateFormat;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -78,6 +79,7 @@ public class InvoiceController {
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		
 		}
 		
 	
